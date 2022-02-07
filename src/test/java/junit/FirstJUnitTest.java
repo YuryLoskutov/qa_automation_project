352078750ef009ed474b3ebe70dbb5d7195baef0
YuryLoskutov <megafudziyama@gmail.com>
2022-02-07 21:19:16 +0300

    Some stuff added

---
+++ src/test/java/junit/FirstJUnitTest.java
@@ -0,0 +1,29 @@
+package junit;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import net.jodah.failsafe.internal.util.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTest {
+
+    @BeforeEach
+    void openBrowser() {
+        Configuration.browser = "firefox";
+        Selenide.open("https://yandex.ru");
+    }
+
+    @AfterEach
+    void closeBrowser(){
+        Configuration.browser = "firefox";
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(2 < 3 );
+    }
+}
