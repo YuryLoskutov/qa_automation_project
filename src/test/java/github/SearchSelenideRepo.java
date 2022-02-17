8b6d7d89b43185e8322fd5ac4b7f2df0dd2c4a11
YuryLoskutov <megafudziyama@gmail.com>
2022-02-17 21:33:48 +0300

    Added SearchSelenide test

---
+++ src/test/java/github/SearchSelenideRepo.java
@@ -0,0 +1,28 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SearchSelenideRepo {
+
+    @Test
+    void shouldFindSelenideGithub() {
+        //открыть страницу github.com
+        open("https://github.com/");
+        // ввести в поле поиска selenide и нажать enter
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
+        // нажимаем на линк первого результата поиска
+        $$("ul.repo-list li").first().$("a").click();
+        // check: в заголовке встречается selenide/selenide
+        $("h1").shouldHave(text("selenide / selenide"));
+    }
+}
+
+// Тест состоит из (запиши в тетрадь):
+// ARRANGE - фаза подготовки данных, например тест данные
+// ACT - выполнение шага
+// ASSERT - проверка рез-тов
