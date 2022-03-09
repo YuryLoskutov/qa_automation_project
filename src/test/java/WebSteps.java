7ba51b0fd453e9507f4115bf3c1213ebe7176056
YuryLoskutov <megafudziyama@gmail.com>
2022-03-09 23:00:24 +0300

    Deleted unused imports

--- src/test/java/WebSteps.java
+++ src/test/java/WebSteps.java
@@ -1,13 +1,6 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.WebDriverRunner;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-
-import java.nio.charset.StandardCharsets;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
