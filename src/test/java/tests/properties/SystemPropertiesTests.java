b66ead5fced1a79887f85d2c49aa37e1ef7009a5
YuryLoskutov <megafudziyama@gmail.com>
2022-05-12 21:44:44 +0300

    Added properties

---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,44 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+
+    @Test
+    void someTest1() {
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+
+    @Test
+    void someTest2() {
+        System.setProperty("browser", "safari");
+        String browser = System.getProperty("browser");
+        System.out.println(browser);
+    }
+
+    @Test
+    void someTest3() {
+
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser);
+    }
+
+    @Test
+    void someTest4() {
+        System.setProperty("browser", "safari");
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser);
+    }
+
+    @Test
+    void someTest5() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "100");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+
+        System.out.println(browser);
+        System.out.println(version);
+        System.out.println(browserSize);
+    }
+}
