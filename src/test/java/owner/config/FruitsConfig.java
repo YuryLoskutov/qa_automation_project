0014b53d19355a1e9c2fe1471d319cf78a98ec99
YuryLoskutov <megafudziyama@gmail.com>
2022-06-13 23:18:40 +0300

    added some stuff from the lesson

---
+++ src/test/java/owner/config/FruitsConfig.java
@@ -0,0 +1,24 @@
+package owner.config;
+
+import org.aeonbits.owner.Config;
+
+import java.util.List;
+
+public interface FruitsConfig extends Config {
+
+    @Key("array")
+    String[] getFruitsArray();
+
+    @Key("list")
+    List<String> getFruitsList();
+
+    @Key("default")
+    @DefaultValue("apple,banana")
+    List<String> getFruitsDefault();
+
+    @Key("separator")
+    @Separator(";")
+    List<String> getFruitsSeparator();
+
+
+}
